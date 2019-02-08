97f43febbc578c706a043580886d6b63075ef6b5
KalliMiika <miika.kallioinen@helsinki.fi>
2019-02-08 07:56:02 +0200

    Kaapelin koko kenttä muutettu integeristä stringiksi

--- application/cables/models.py
+++ application/cables/models.py
@@ -12,7 +12,7 @@ class Cable(db.Model):
     #Kaapelin toisessa päässä oleva Risteyskoje 'B'
     controller_b_id = db.Column(db.Integer, db.ForeignKey('controller.id'), nullable=False)
     #Kaapelin kapasiteetti
-    size = db.Column(db.Integer, nullable=False)
+    size = db.Column(db.String(144), nullable=False)
 
     #Kaapelin nimi
     name = db.Column(db.String(144), nullable=False)
